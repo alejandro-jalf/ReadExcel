1ec69034cb3b0e8e363de96019674b2fa1ed032e
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-10-06 10:52:18 -0500

    Creacion de primer avance

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        registers: [
+            // {Articulo: "0541137", Nombre: "Pas. Triple Accion Ext. B Colgate 50 ml", Factor: "1/44", Unidad: "CJA/PZA", Pedido: 1, Viveres: 1320.70},
+            // {Articulo: "0541137", Nombre: "Pas. Triple Accion Ext. B Colgate 60 ml", Factor: "1/44", Unidad: "CJA/PZA", Pedido: 1, Viveres: 1320.70},
+            // {Articulo: "0541137", Nombre: "Pas. Triple Accion Ext. B Colgate 70 ml", Factor: "1/44", Unidad: "CJA/PZA", Pedido: 1, Viveres: 1320.70},
+            // {Articulo: "0541137", Nombre: "Pas. Triple Accion Ext. B Colgate 80 ml", Factor: "1/44", Unidad: "CJA/PZA", Pedido: 1, Viveres: 1320.70}
+        ]
+    },
+    methods: {
+
+    }
+});
